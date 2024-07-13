75b23a55571630a68419dcd674bee29c2ad3f332
Sailesh Pennam <109687469+Dev-NeoGraphx@users.noreply.github.com>
2024-07-13 17:21:00 +0530

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,4 +1,8 @@
-$_GET https://www.neographx.com/webhooks/
-  hub.mode=subscribe&
-  hub.challenge=0411196124&
-  hub.verify_token=nagendraraopennam
+<?php
+$hubVerifyToken = 'ngwhatsapp_webhooks';
+$accessToken = "";
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['hub_challenge']) && $_GET['hub_verify_token' ===$hubVerifyToken]){
+  echo $_GET['hub_challenge'];
+  exit;
+}
+?>
